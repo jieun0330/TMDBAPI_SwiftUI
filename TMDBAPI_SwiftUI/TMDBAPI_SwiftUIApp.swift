d76ecdaf10b1586af237fe927d9e291515e81ff7
jieun <jieun_0330@naver.com>
2024-05-12 17:47:57 +0900

    init: initial commit

---
+++ TMDBAPI_SwiftUI/TMDBAPI_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDBAPI_SwiftUIApp.swift
+//  TMDBAPI_SwiftUI
+//
+//  Created by 박지은 on 5/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TMDBAPI_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
